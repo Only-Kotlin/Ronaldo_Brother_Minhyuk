e67ef52b982ea88c4329cf7e1870459f86845433
정민혁 <jlssteeve1@gmail.com>
2021-12-31 15:02:52 +0900

    Add level 1 lotto

--- app/src/main/java/com/app/ronaldobrotherminhyuk/level_1/Lotto.kt
+++ app/src/main/java/com/app/ronaldobrotherminhyuk/level_1/Lotto.kt
@@ -7,29 +7,42 @@ class Solution {
         var noDangChum = 0
 
 
-        for(lotto in lottos.indices){
-            if(lottos[lotto] == 0){
-                noDangChum+=1
+        for (lotto in lottos.indices) {
+            if (lottos[lotto] == 0) {
+                noDangChum += 1
             }
-            for(win_num in win_nums.indices){
-                if(lottos[lotto] == win_nums[win_num]){
-                    dangChum+=1
+            for (win_num in win_nums.indices) {
+                if (lottos[lotto] == win_nums[win_num]) {
+                    dangChum += 1
                 }
             }
         }
-        var answers: IntArray = intArrayOf(dangChum+noDangChum,dangChum)
+        var answers: IntArray = intArrayOf(dangChum + noDangChum, dangChum)
 
-        for(answer in answers.indices){
+        for (answer in answers.indices) {
             when {
-                answers[answer] == 6 -> {answers[answer] = 1}
-                answers[answer] == 5 -> {answers[answer] = 2}
-                answers[answer] == 4 -> {answers[answer] = 3}
-                answers[answer] == 3 -> {answers[answer] = 4}
-                answers[answer] == 2 -> {answers[answer] = 5}
-                answers[answer] == 1 -> {answers[answer] = 6}
-                answers[answer] == 0 -> {answers[answer] = 6}
+                answers[answer] == 6 -> {
+                    answers[answer] = 1
+                }
+                answers[answer] == 5 -> {
+                    answers[answer] = 2
+                }
+                answers[answer] == 4 -> {
+                    answers[answer] = 3
+                }
+                answers[answer] == 3 -> {
+                    answers[answer] = 4
+                }
+                answers[answer] == 2 -> {
+                    answers[answer] = 5
+                }
+                answers[answer] == 1 -> {
+                    answers[answer] = 6
+                }
+                answers[answer] == 0 -> {
+                    answers[answer] = 6
+                }
             }
-
         }
         return answers
     }
