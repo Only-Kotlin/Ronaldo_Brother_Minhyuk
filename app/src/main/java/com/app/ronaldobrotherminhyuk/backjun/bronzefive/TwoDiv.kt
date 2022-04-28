9f0d73e88e6ba010744cdffbd81e19b031109f3d
minhyuuk <jlssteeve1@gmail.com>
2022-04-28 11:53:39 +0900

    [UPDATE] 백준 브론즈 5레벨 1008번

---
+++ app/src/main/java/com/app/ronaldobrotherminhyuk/backjun/bronzefive/TwoDiv.kt
@@ -0,0 +1,7 @@
+package com.app.ronaldobrotherminhyuk.backjun.bronzefive
+
+import java.util.*
+
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
+    println(String.format("%.9f", nextDouble().div(nextDouble())))
+}
