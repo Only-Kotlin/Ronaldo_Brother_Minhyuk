e7bb2f1310bcd5c24c6fedf0377b4662957c1075
minhyuuk <jlssteeve1@gmail.com>
2022-04-30 02:23:29 +0900

    [UPDATE] 브론즈 5레벨 2557번 출력하기

---
+++ app/src/main/java/com/app/ronaldobrotherminhyuk/backjun/bronze/2557.kt
@@ -0,0 +1,16 @@
+package com.app.ronaldobrotherminhyuk.backjun.bronze
+
+fun main(args:Array<String>){
+//    입력을 하는 경우
+//    val br = System.`in`.bufferedReader()
+//    val bw = System.out.bufferedWriter()
+//
+//    bw.write(br.readLine())
+//
+//    bw.flush()
+//    bw.close()
+//    br.close()
+
+//    그냥 간단하게 출력만 하면 됨
+    println("Hello World!")
+}
