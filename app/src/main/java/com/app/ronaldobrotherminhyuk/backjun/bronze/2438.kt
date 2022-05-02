fd3c088e6fe140127df297b538dc7b061a9e2d8e
minhyuuk <jlssteeve1@gmail.com>
2022-05-02 18:17:54 +0900

    [UPDATE] 브론즈 3레벨 2438 별 찍기

---
+++ app/src/main/java/com/app/ronaldobrotherminhyuk/backjun/bronze/2438.kt
@@ -0,0 +1,15 @@
+package com.app.ronaldobrotherminhyuk.backjun.bronze
+
+fun main(args:Array<String>){
+    val br = System.`in`.bufferedReader()
+    // 5
+    val token = br.readLine().toInt()
+
+    for(i: Int in 1..token){
+        for(j: Int in 1..i){
+            print("*")
+        }
+        println("")
+    }
+    br.close()
+}
