8727de353bc6cc2c2548c3ed1deed752d84a16b5
minhyuuk <jlssteeve1@gmail.com>
2022-05-02 11:56:46 +0900

    [UPDATE] 브론즈 5레벨 11654번 아스키코드

---
+++ app/src/main/java/com/app/ronaldobrotherminhyuk/backjun/bronze/11654.kt
@@ -0,0 +1,8 @@
+package com.app.ronaldobrotherminhyuk.backjun.bronze
+
+fun main(args:Array<String>){
+    val br = System.`in`.bufferedReader()
+    val token = br.readLine()
+
+    println(token[0].toInt())
+}
