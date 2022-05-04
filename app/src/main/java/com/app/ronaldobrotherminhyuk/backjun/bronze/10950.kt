ad10043a6fe86e0d463258d9fe594e769e96be23
minhyuuk <jlssteeve1@gmail.com>
2022-05-04 23:54:03 +0900

    [UPDATE] 브론즈 3레벨 10950 A+B - 3

---
+++ app/src/main/java/com/app/ronaldobrotherminhyuk/backjun/bronze/10950.kt
@@ -0,0 +1,10 @@
+package com.app.ronaldobrotherminhyuk.backjun.bronze
+
+import java.util.*
+
+fun main(args:Array<String>) = with(Scanner(System.`in`)){
+
+    for(i in 1..nextInt()){
+        println(nextInt() + nextInt())
+    }
+}
