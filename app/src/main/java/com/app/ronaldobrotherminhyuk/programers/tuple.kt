e68e6520361b5d8a841d5a0fe5c94243a9d9942b
minhyuuk <jlssteeve1@gmail.com>
2022-05-11 08:42:11 +0900

    [UPDATE] 코테 전 마지막 기본(?)정리

---
+++ app/src/main/java/com/app/ronaldobrotherminhyuk/programers/tuple.kt
@@ -0,0 +1,12 @@
+package com.app.ronaldobrotherminhyuk.programers
+
+class Solution {
+    fun solution(s: String): IntArray {
+        var answer = intArrayOf()
+        val numbers = s.split('{','}',',')
+
+
+
+        return answer
+    }
+}
